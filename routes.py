0936516ccd7b36fb7c2c4183f57bdf782886f7e2
jxwu1 <jxwu2002@gmail.com>
2026-04-18 15:39:56 +0300

    feat: register import blueprint and add nav item

--- routes.py
+++ routes.py
@@ -1,3 +1,4 @@
+from routes_import import bp as import_bp
 from routes_pages_tasks import bp as pages_tasks_bp
 from routes_query import bp as query_bp
 
@@ -7,6 +8,7 @@ from config import CONFIG
 def register_routes(app) -> None:
     app.register_blueprint(pages_tasks_bp)
     app.register_blueprint(query_bp)
+    app.register_blueprint(import_bp)
     if CONFIG.dual_mode:
         from routes_transfer import bp as transfer_bp
         from routes_collab import bp as collab_bp
